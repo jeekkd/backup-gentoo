b747a194ab3749b1ea6fee7e79d800c1cc5a5a85
turkgrb <daulton@protonmail.com>
2016-02-25 13:31:54 -0500

    Added some extra handling for grub config installation, changed header comment slightly.

--- chroot_commands.sh
+++ chroot_commands.sh
@@ -2,8 +2,8 @@
 
 # Written by: https://github.com/turkgrb
 # Website: https://daulton.ca
-# These are the commands that will be ran in the chroot when the 
-# system is restored using the primary backup_gentoo.sh script
+# Purpose: To make the backup and restoration of Gentoo systems easier, uses gzip
+# tars and can also handle GRUB installation too with the restoration process.
 # Note: This is the commands the main script run within the chroot environment
 
 env-update 
@@ -11,12 +11,18 @@ source /etc/profile
 export PS1="(chroot) $PS1" 
 		
 echo "Beginning to sync and emerge grub.."
-sudo emerge --sync
-sudo emerge --ask sys-boot/grub:2
-		
+sudo emerge-webrsync
+sudo emerge sys-boot/grub:2
+	
+# Sometimes grub saves new config with .new extension
+# so this is assuring that an existing config is removed
+# and the new one is renamed after installation so it can
+# be used properly		
 echo "Installing grub to disk and making config.."
 sudo grub2-install $1
+sudo rm -rf /boot/grub/grub.cfg
 sudo grub2-mkconfig -o /boot/grub/grub.cfg
+sudo mv /boot/grub/grub.cfg.new /boot/grub/grub.cfg
 
 echo "Cleaning up.."
 sudo rm -rf chroot_commands.sh
